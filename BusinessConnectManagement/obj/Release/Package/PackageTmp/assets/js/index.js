3969b13e09198d272b27bbdd78c7ac3cabe2e2d1
LongLuu <64773511+LongLuu33931@users.noreply.github.com>
2023-03-31 17:16:08 +0700

    long push code

--- BusinessConnectManagement/obj/Release/Package/PackageTmp/assets/js/index.js
+++ BusinessConnectManagement/obj/Release/Package/PackageTmp/assets/js/index.js
@@ -1,11 +1,4 @@
-﻿//// TOAST
-//let toastIcon = document.querySelector(".toast-right-icon");
-
-//toastIcon.addEventListener("click", function () {
-//    document.querySelector(".toast").classList.add("is-hide");
-//});
-
-// MenuIcon -> arrow
+﻿// MenuIcon -> arrow
 let menu = document.querySelector(".menu");
 let closeBtn = document.querySelector("#menu-icon");
 
